de2e2f6fd2748dd71bdf8111dcfe7e825e4d6723
jaylacamiling <jayla.camiling@gmail.com>
2025-03-06 20:50:03 +0000

    Thu, Mar 6, 2025, 12:50 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -12,7 +12,7 @@ function blurring() {
     if (load > 99){
         clearInterval(int)
     }
-    loadText.innerText = `$(load)%`
+    loadText.innerText = `${load}%`
     loadText.style.opacity = 1 - load/100
 
     bg.style.filter = `blur(${30-(load/100)*30}px)`
