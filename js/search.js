68101bd83fdff54ed54e700caa0e6f7dd804a494
jaylacamiling <jayla.camiling@gmail.com>
2025-03-06 21:25:16 -0800

    search

--- js/search.js
+++ js/search.js
@@ -1,8 +1,6 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+btn = document.querySelector('.btn')
+search = document.querySelector('.search')
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
-    input.focus()
 })
