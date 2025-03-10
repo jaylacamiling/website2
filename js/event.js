28f20c7e935c060718063e6261e1efe2b1ed4156
jaylacamiling <jayla.camiling@gmail.com>
2025-03-09 21:24:34 -0700

    Initial commit

--- js/event.js
+++ js/event.js
@@ -1,32 +1,72 @@
-else if (event.key == 'ArrowRight'){
-    positionX = positionX - 10
-}
-element3.style.top = `${positionY}px`
-element3.style.right = `${positionX}px`
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+
+element1.addEventListener('click', ()=>{
+    element1.innerHTML = "I've been clicked! <br> Try double clicking me."
+})
+
+element1.addEventListener('dblclick', ()=>{
+    element1.innerHTML = "I've been double clicked!<br> Try single clicking me."
 })
 
 
-element4.addEventListener('click',()=>{
-element4.style.width = "10px"
-element4.style.height = "10px"
-element4.innerHTML = ""
-element4.style.opacity = .3
+element2.addEventListener('mouseover', ()=>{
+    element2.style.color = "green"
+    element2.style.backgroundColor = "blue"
+})
+
+element2.addEventListener('mouseout', ()=>{
+    element2.style.color = "white"
+    element2.style.backgroundColor = "BB33BB"
+})
+
+let positionY = 0;
+let positionX = 0;
+
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key)
+
+    if (event.key == 'ArrowDown') {
+        positionY = positionY + 10
+    }
+    else if (event.key == 'ArrowUp') {
+        positionY = positionY - 10
+    }
+    else if (event.key == 'ArrowLeft') {
+        positionX = positionX + 10
+    }
+    else if (event.key == 'ArrowRight') {
+        positionX = positionX - 10
+    }
+    element3.style.top = `${positionY}px`
+    element3.style.right = `${positionX}px`
+})
+
+
+
+element4.addEventListener('click', ()=>{
+    element4.style.width = "10px"
+    element4.style.height = "10px"
+    element4.innerHTML = ""
+    element4.style.opacity = .3
 })
 
 
 document.addEventListener('keyup', (event)=>{
-if (event.key == 'j'){
-    element4.style.width = "300px"
-    element4.style.height = "50px"
-    element4.style.opacity = 1
-    element4.innerHTML = "<h4>Click me to make me small. <br> Press 'j' to bring me back. </h4>"
-}
+    if (event.key == 'j'){
+        element4.style.width = "300px"
+        element4.style.height = "50px"
+        element4.style.opacity = 1
+        element4.innerHTML = "<h4>Click me to make me small. <br> Press 'j' to bring me back.</h4>"
+    }
 })
 
 
 const list = ['running', 'paused']
 let index = 0
-element5.addEventListener('click',()=>{
-element5.style.animationPlayState = list[index]
-index = (index + 1) % 2
+element5.addEventListener('click', ()=>{
+    element5.style.animationPlayState = list[index]
+    index = (index + 1) % 2
 })
