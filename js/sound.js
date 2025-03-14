81ef370e45361bb4edc97e62506699ae5ef2424f
jaylacamiling <jayla.camiling@gmail.com>
2025-03-13 20:45:25 -0700

    Updated website

---
+++ js/sound.js
@@ -0,0 +1,21 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound =>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        document.getElementById(sound).pause()
+        document.getElementById(sound).currentTime = 0;
+    })
+}
