473a820d882e187885143c2c6d13ae25d84e64c9
jaylacamiling <jayla.camiling@gmail.com>
2025-04-05 21:13:03 -0700

    Updated website

---
+++ js/ball.js
@@ -0,0 +1,36 @@
+const ball = document.createElement('div')
+const ballRadius = 30
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+let ballXPosition = windowWidth/2 - ballRadius
+let ballYPosition = windowHeight/2 - ballRadius
+let ballSpeed = 5
+let ballXDirection = 1
+let ballYDirection = 1
+
+setInterval(moveBall, 10)
+
+function moveBall(){
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ball.style.left = `${ballXPosition}px`
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowWidth - 2 * ballRadius){
+        ballYDirection = ballYDirection -1
+    }
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius){
+        ballXDirection = ballXDirection * -1
+    }
+
+}
+
+createBall()
+function createBall(){
+    document.body.appendChild(ball)
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "green"
+    ball.style.position = "absolute"
+    ball.style.top = `${ballYPosition}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
+}
